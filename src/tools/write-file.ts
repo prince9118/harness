a73937bd08da8666cda3c7251fa4830ceb47615b
prince9118 <pchaudharyskn@gmail.com>
2026-08-12 00:33:24 +0530

    write file tool

--- src/tools/write-file.ts
+++ src/tools/write-file.ts
@@ -2,5 +2,20 @@ import { writeFile } from "node:fs/promises";
 import type { Tool } from "./types";
 
 export const writeFileTool:Tool={
-    
+    name:"write_file",
+    description:"Create or overwrite a file with the provided contet.",
+
+    async execute(args){
+        const path =args.path;
+        const content= args.content;
+        if(typeof path !== "string"){
+            throw new Error("path must be a string");
+        }
+        if(typeof content !== "string"){
+            throw new Error("content must be a string");
+        }
+        await writeFile(path, content,"utf-8");
+        return `Successfully wrote ${path}`;
+    }
+
 }
